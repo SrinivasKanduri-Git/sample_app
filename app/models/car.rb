7e208aa1a259ab9d35812475fc2da44a1de11e36
Srinivas Kanduri <srinivaskanduri03@gmail.com>
2024-07-26 14:43:50 +0530

    Added validations to Car, modified root route.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,4 @@
 class Car < ApplicationRecord
+  validates :model, presence: true, length: {minimum:3, maximum:10}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
