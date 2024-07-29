722d9da4c0c72ca1d5716583b345792e66b8cd5a
Srinivas Kanduri <srinivaskanduri03@gmail.com>
2024-07-29 10:55:20 +0530

    Added user model, created a controller and view folder.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_071601) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_29_050112) do
   create_table "cars", force: :cascade do |t|
     t.string "model"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
